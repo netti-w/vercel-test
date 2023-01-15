d0e21a41b62edd440386d1896c36b194d5b2943d
netti-w <106514885+netti-w@users.noreply.github.com>
2023-01-15 16:52:51 +0100

    Import and connect to local database

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const uuid = require('uuid');
 const { check, validationResult } = require('express-validator');
 
 const mongoose = require('mongoose');
+const Models = require('./models.js'); //require (import) 'custom' models.js
+
+// call movie and user modals from modals.js
+const Movies = Models.Movie;
+const Users = Models.User;
+
+// Connecting LOCAL myFlixDB via Mongoose to perform CRUD operations
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 const port = 3000;
