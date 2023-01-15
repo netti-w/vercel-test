3d0effce7b7736dd7211711d782d98ef91d88116
netti-w <106514885+netti-w@users.noreply.github.com>
2023-01-15 16:33:48 +0100

    Import libraries

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
+const { check, validationResult } = require('express-validator');
 const app = express();
 const port = 3000;
+
+const cors = require('cors');
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
