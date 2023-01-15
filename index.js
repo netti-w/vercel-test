b139cebf5659c013e09737803c14f9c83c44914a
netti-w <106514885+netti-w@users.noreply.github.com>
2023-01-15 16:38:04 +0100

    Add static page

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const port = 3000;
 
 const cors = require('cors');
 
+// function serving all requests of static file (here:"documenation.html") from public folder
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
