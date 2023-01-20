ac8d0159738db94dbcd46a904bf96881b63a0f36
netti-w <106514885+netti-w@users.noreply.github.com>
2023-01-20 22:35:18 +0100

    Add user endpoints without auth

--- index.js
+++ index.js
@@ -41,8 +41,10 @@ app.use(cors({
   }
 }));
 
-// function serving all requests of static file (here:"documenation.html") from public folder
-app.use(express.static('public'));
+let auth = require('./auth')(app);
+const passport = require('passport');
+require('./passport');
+
 
 // ----------------------- Homepage -----------------------
 app.get('/', (req, res) => {
@@ -106,6 +108,183 @@ app.get('/movies/directors/:directorName', (req, res) => {
     });
 });
 
+// ----------------------- User endpoints -----------------------
+
+/**
+ * GET the list of all users
+ * @returns an array of all user objects in JSON format
+ */
+app.get('/users', (req, res) => {
+  Users.find().then(users => res.json(users));
+});
+
+/**
+ * GET data about a single user by name
+ * @params {string} Username
+ * @returns a new user object in json format
+ */
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+/**
+ * POST data creating a new user
+ * @params {string} Username
+ * @params {string} Password
+ * @params {string} Email
+ * @returns a new user object in JSON format
+ */
+app.post('/users',
+  [
+    check('Username', 'Username is required (min. 5 characters)').isLength({ min: 5 }),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+    // check the validation object for errors
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + ' already exists');
+        } else {
+          Users.create({
+            Username: req.body.Username,
+            Password: hashedPassword,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+            .then((user) => { res.status(201).json(user) })
+            .catch((error) => {
+              console.error(error);
+              res.status(500).send('Error: ' + error);
+            })
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
+
+/**
+ * PUT data updating a user's info by user name
+ * @params {string} Username
+ * @params {string} Password
+ * @params {string} Email
+ * @returns a new user object in JSON format
+ */
+app.put('/users/:Username',
+  [
+    check('Username', 'Username is required').isLength({ min: 5 }),
+    check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+    check('Password', 'Password is required').not().isEmpty(),
+    check('Email', 'Email does not appear to be valid').isEmail()
+  ], (req, res) => {
+    let errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    let hashedPassword = Users.hashPassword(req.body.Password);
+    Users.findOneAndUpdate({ Username: req.params.Username },
+      {
+        $set: {
+          Username: req.body.Username,
+          Password: hashedPassword,
+          Email: req.body.Email,
+          Birthday: req.body.Birthday
+        }
+      },
+      { new: true }, // This line makes sure that the updated document is returned
+      (err, updatedUser) => {
+        if (err) {
+          console.error(err);
+          res.status(500).send('Error: ' + err);
+        } else {
+          res.json(updatedUser);
+        }
+      });
+  });
+
+/**
+ * PUT data adding a user's favourite movie to a list
+ * @params {string} Username
+ * @params {string} MovieID
+ * @returns a new user object in JSON format
+ */
+app.put('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $push: { FavouriteMovies: req.params.MovieID }
+  },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+});
+
+/**
+ * DELETE data removing a user's favourite movie from the list
+ * @params {string} Username
+ * @params {string} MovieID
+ */
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+  Users.findOneAndUpdate({ Username: req.params.Username }, {
+    $pull: { FavouriteMovies: req.params.MovieID }
+  },
+    { new: true }, // This line makes sure that the updated document is returned
+    (err, updatedUser) => {
+      if (err) {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      } else {
+        res.json(updatedUser);
+      }
+    });
+});
+
+/**
+ * DELETE data removing a user by user name
+ * @params {string} Username
+ * @returns success message
+ */
+app.delete('/users/:Username', (req, res) => {
+  Users.findOneAndRemove({ Username: req.params.Username })
+    .then((user) => {
+      if (!user) {
+        res.status(400).send(req.params.Username + ' was not found');
+      } else {
+        res.status(200).send(req.params.Username + ' was deleted.');
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+
+// function serving all requests of static file (here:"documenation.html") from public folder
+// app.use(express.static('public'));
 
 app.use(express.static('public'));
 app.get('/documentation', (req, res) => {
