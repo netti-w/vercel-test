c7989fa3eb91c36de8191cc4751089ea8bf58e48
netti-w <106514885+netti-w@users.noreply.github.com>
2023-01-20 18:39:35 +0100

    Change to bcryptjs

--- models.js
+++ models.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 
 let movieSchema = mongoose.Schema({
   Title: { type: String, required: true },
@@ -30,12 +30,12 @@ let userSchema = mongoose.Schema({
 
 // Function hashing submitted password
 userSchema.statics.hashPassword = (password) => {
-  return bcrypt.hashSync(password, 10);
+  return bcryptjs.hashSync(password, 10);
 };
 
 // Function comparing submitted hashed password with hashed password in DB
 userSchema.methods.validatePassword = function (password) {
-  return bcrypt.compareSync(password, this.Password);
+  return bcryptjs.compareSync(password, this.Password);
 };
 
 let Movie = mongoose.model('Movie', movieSchema);
