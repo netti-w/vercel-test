6022e0dd7ede1a76be5c20b727b4c3c0eaa157bf
netti-w <106514885+netti-w@users.noreply.github.com>
2023-01-20 19:09:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // CORS - allowing requests from other specified origins (here: default all origins)
 const cors = require('cors');
-app.use(cors());
+// app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a origin is not on the list of allowed origins
+      let message = 'The CORS policy for this application doesn\'t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // function serving all requests of static file (here:"documenation.html") from public folder
 app.use(express.static('public'));
